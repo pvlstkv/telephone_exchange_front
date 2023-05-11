6c142f55d8e277af1384529611e273a9b6e7143e
pavel astukov <p7ll7p@google.com>
2023-05-11 21:31:47 +0400

    simple login form

---
+++ src/login/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { redirect } from "react-router-dom";
+
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (username === 'admin' && password === 'password') {
+      console.log('Login successful!');
+      return redirect("/")
+    } else {
+      console.log('Invalid username or password');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default LoginForm;

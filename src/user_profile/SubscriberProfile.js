1a553bf4f754df2812a1987fd37093aa0b737126
pavel astukov <p7ll7p@google.com>
2023-05-16 23:17:53 +0400

    almost done profile

--- src/user_profile/SubscriberProfile.js
+++ src/user_profile/SubscriberProfile.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import jwtDecode from 'jwt-decode';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
+import Select from 'react-select'
 
 function SubscriberProfile (){
   const [user, setUser] = useState({});
   const [oldUser, setOldUser ] = useState({})
   const [editMode, setEditMode] = useState(false);
   let jwt = localStorage.getItem('token')
-  
+
   const location = useLocation();
   const {userId} = location.state
   console.log('userId is ', userId)
@@ -20,25 +21,38 @@ function SubscriberProfile (){
   }  
   useEffect(() => {
             const fetchUser = async () => {
-            const response = await axios.get(`http://localhost:8080/subscribers/${userId}`, axiosConfig);
-            setUser(response.data);
-            setOldUser(response.data)
+            axios.get(`http://localhost:8080/subscribers/${userId}`, axiosConfig)
+            .then(response=>{
+              setUser(response.data);
+              setOldUser(response.data)
+
+              const fetchPhoneNumbers= async ()=>{
+                  let ids = response.data.phoneNumberIds.join(',')
+                  axios.get(`http://localhost:8080/phone-numbers/${ids}`, axiosConfig)
+                  .then(response=>{
+                      setPhoneNumbers(response.data)
+                      const selected = response.data.map(item=>({
+                        value:item.id,
+                        label:item.phone
+                      }))
+                      setSelectedNumbers(selected)
+                      setOldSelectedNUmbers(selected)
+                      console.log('selected', selected)
+                  })                  
+                }
+              fetchPhoneNumbers()
+            })
         };
         fetchUser();
     }, [])
 
   const [phoneNumbers, setPhoneNumbers] = useState([])
-  useEffect(()=>{
-    async function fetchPhoneNumbers(){
-      if (user.phoneNumberIds && user.phoneNumberIds.length > 0) {
-        let ids = user.phoneNumberIds.join(',')
-        const response = await axios.get(`http://localhost:8080/phone-numbers/${ids}`, axiosConfig)
-        setPhoneNumbers(response.data)
-        console.log(response.data)
-      }
-    }
-    fetchPhoneNumbers()
-  }, [user])
+  const [oldSelectedNumbers, setOldSelectedNUmbers] = useState()
+  const[selectedNumbers, setSelectedNumbers] = useState([])
+
+  // useEffect(()=>{
+    
+  // }, [user])
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -52,24 +66,24 @@ function SubscriberProfile (){
             )
         return
     }
-    if (name === 'phoneNumberIds') {
-      const ids = value.split(',').map((id) => parseInt(id.trim()));
-      setUser({
-        ...user,
-        phoneNumberIds: ids,
-      });
-      return;
-    }
-
     console.log(name);
     console.log(user)
     setUser({ ...user, [name]: value });
   };
-
+  const handleNumberChange=(selectedNumber)=>{
+      console.log('selectedNUmber is', selectedNumber)
+      setSelectedNumbers(selectedNumber)
+      setUser({
+        ...user,
+        phoneNumberIds:selectedNumbers.map(item=>item.value)
+      })
+      console.log('user phones are', user.phoneNumberIds)
+  }
 
   function handleCancelEdit(event){
     setEditMode(false)
     setUser(oldUser)
+    setSelectedNumbers(oldSelectedNumbers)
   }
 
     const handleSave = async (event) => {
@@ -104,6 +118,22 @@ function SubscriberProfile (){
    
   };
 
+
+  const [allPhones, setAllPhones] = useState([])
+  const handleEditMode= ()=> {
+    axios.get('http://localhost:8080/phone-numbers', axiosConfig)
+    .then(response=>{
+      const options = response.data.map(item=>({
+        
+          value:item.id,
+          label:item.phone
+        
+      }))
+      setAllPhones(options)
+      console.log('all phone',options)
+    })
+    setEditMode(true);
+  }
 
   return (
     <div>
@@ -202,6 +232,18 @@ function SubscriberProfile (){
       }   
       </div>
       <div>
+        <label>телефонные номера:</label>
+        {editMode ? (
+          <Select options={allPhones} value={selectedNumbers} isMulti onChange={handleNumberChange}></Select>
+        ):(
+          <span>{<ul>
+            {phoneNumbers && phoneNumbers.map((item, idx) => (
+              <li key={idx}>{item.phone}</li>
+            ))}
+          </ul>}</span>
+        )}
+      </div>
+      <div>
         <label>Role:</label>
         {editMode ? (
             <select
@@ -224,10 +266,12 @@ function SubscriberProfile (){
             <button onClick={handleSave}>Save</button>
         </div>
       ) : (
-        <button onClick={() => setEditMode(true)}>Edit</button>
+        <button onClick={handleEditMode}>Edit</button>
       )}
     </div>
   );
+
+  
 };
 
 export default SubscriberProfile;
